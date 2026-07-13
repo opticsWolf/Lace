14e88e4e878efe5d478ac453055b524d6d42f931
opticsWolf <f.wolff2010@gmail.com>
2026-07-13 20:01:24 +0200

    header update

--- demo_app.py
+++ demo_app.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Lace: Advanced PySide6 Docking System
-Copyright (c) 2026 opticsWolf
-
-SPDX-License-Identifier: Apache-2.0  G
-"""
+# Lace: Advanced PySide6 Docking System
+# Copyright (c) 2026 opticsWolf
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# This file is part of Lace.
+# Licensed under the Apache License, Version 2.0.
 
 import sys
 import logging
