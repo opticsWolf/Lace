47acc4a407d66d19055d08ff512cae2af3f1a345
opticsWolf <opticswolf@protonmail.com>
2026-08-07 07:38:18 +0200

    chore(release): bump version to 0.4.5

--- lace/__init__.py
+++ lace/__init__.py
@@ -14,7 +14,7 @@ Public API is imported explicitly at the top level for discoverability
 and IDE/mypy compatibility.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.5"
 
 # ---------------------------------------------------------------------------
 # Core Classes
